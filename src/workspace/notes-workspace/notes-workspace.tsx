c35b6ee16ef36873fbf6f2f02b63d9bbbf026632
Milena <morozowamilena@yandex.ru>
2022-05-23 16:48:42 +0300

    added action panel to workspace

--- src/workspace/notes-workspace/notes-workspace.tsx
+++ src/workspace/notes-workspace/notes-workspace.tsx
@@ -1,15 +1,19 @@
 import BoardType from '../../entities/board-type';
+import ActionPanel from '../action-panel/action-panel';
 import WorkspaceAppBar from '../app-bar/app-bar';
 
 function NotesWorkspace() {
   const onSearch = (text: string) => {};
 
   return (
-    <WorkspaceAppBar
-      placeholder="search note by tag"
-      boardType={BoardType.NOTES}
-      search={onSearch}
-    />
+    <>
+      <WorkspaceAppBar
+        placeholder="search note by tag"
+        boardType={BoardType.NOTES}
+        search={onSearch}
+      />
+      <ActionPanel/>
+    </>
   );
 }
 
