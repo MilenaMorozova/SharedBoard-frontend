9d7f203911926d0f0d119706feba6237d90a7172
Milena <morozowamilena@yandex.ru>
2022-06-05 22:41:36 +0300

    fix proplem 'FormControl.js:176 MUI: There are multiple  components inside a FormControl'

--- src/workspace/app-bar/share-board-dialog/access-select.tsx
+++ src/workspace/app-bar/share-board-dialog/access-select.tsx
@@ -1,22 +1,24 @@
-import { MenuItem, Select } from '@mui/material';
+import { FormControl, MenuItem, Select } from '@mui/material';
 import Access from '../../../entities/user/access';
-import { AccessSelectStyle, MenuItemStyle } from './style';
+import { AccessFormControlStyle, AccessSelectStyle, MenuItemStyle } from './style';
 
 
 function AccessSelect(props: {value: Access, onChange: (access: Access) => void}) {
   return (
-    <Select
-      sx={AccessSelectStyle}
-      size="small"
-      variant="standard"
-      value={props.value}
-      onChange={(event) => props.onChange(event.target.value as Access)}
-      displayEmpty
-      disableUnderline
-    >
-      <MenuItem sx={MenuItemStyle} value={Access.VIEWER}>{Access.VIEWER}</MenuItem>
-      <MenuItem sx={MenuItemStyle} value={Access.EDITOR}>{Access.EDITOR}</MenuItem>
-    </Select>
+    <FormControl sx={AccessFormControlStyle}>
+      <Select
+        sx={AccessSelectStyle}
+        size="small"
+        variant="standard"
+        value={props.value}
+        onChange={(event) => props.onChange(event.target.value as Access)}
+        displayEmpty
+        disableUnderline
+      >
+        <MenuItem sx={MenuItemStyle} value={Access.VIEWER}>{Access.VIEWER}</MenuItem>
+        <MenuItem sx={MenuItemStyle} value={Access.EDITOR}>{Access.EDITOR}</MenuItem>
+      </Select>
+    </FormControl>
   );
 }
 
